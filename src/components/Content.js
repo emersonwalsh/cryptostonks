9ca991f17469631f9433196331814fcddc30954d
Emerson Walsh <emersonwalsh@gmail.com>
2021-09-17 17:55:57 -0400

    added ticker

--- src/components/Content.js
+++ src/components/Content.js
@@ -3,8 +3,9 @@ import NftCard from './NftCard';
 import Footer from './Footer';
 import nfts from './../nft/nfts';
 import IconButton from '@material-ui/core/IconButton';
-
 import TwitterIcon from '@material-ui/icons/Twitter';
+
+import Ticker from 'react-ticker';
 
 export default function Content(props) {
     const handleClick = () => {
@@ -13,11 +14,17 @@ export default function Content(props) {
 
     return (
         <div className={styles.fullPage}>
+
+            <div className={styles.ticker}>
+                <Ticker>
+                    {({ index }) => (<h2 className={styles.tickerItem}>${nfts[index % nfts.length].ticker}</h2>)}
+                </Ticker>
+            </div>
+
             <div className={styles.nav}>
                 <IconButton aria-label="twitter" onClick={handleClick}>
                     <TwitterIcon />
                 </IconButton>
-
             </div>
 
             <div className={styles.grid}>
@@ -27,6 +34,8 @@ export default function Content(props) {
                     </div>
                 ))}
             </div> 
+
+            
 
             <Footer />       
         </div>
